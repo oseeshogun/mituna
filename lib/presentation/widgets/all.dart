cd79f990d0e90965b5a52eadab7c584203a956d4
osee-schoolap <128375152+osee-schoolap@users.noreply.github.com>
2023-08-18 02:27:29 +0100

    feat: :sparkles: add categories list in home page

--- lib/presentation/widgets/all.dart
+++ lib/presentation/widgets/all.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:mituna/core/enums/all.dart';
 import 'package:mituna/core/theme/colors.dart';
 import 'package:mituna/presentation/utils/painters/all.dart';
+import 'package:mituna/presentation/widgets/texts/all.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 part 'primary_button.dart';
 part 'topaz_icon.dart';
 part 'fade_animation.dart';
 part 'running_man_lottie_button.dart';
+part 'category_item.dart';
