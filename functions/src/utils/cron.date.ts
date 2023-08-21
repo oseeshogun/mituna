90d55b1c03f2084560ddb90a1a3ccbd114524dfe
oseeshogun <omasuaku@gmail.com>
2023-08-21 08:25:35 +0100

    :sparkles: (cloud_functions) Deployed cloud functions

--- functions/src/utils/cron.date.ts
+++ functions/src/utils/cron.date.ts
@@ -1,31 +1,31 @@
-// import { parseExpression } from 'cron-parser'
+import { parseExpression } from 'cron-parser'
 
-// export class CronDateUtils {
-//   static getMonthGreaterDate = (): Date => {
-//     const now = new Date()
-//     const daysInMonth = new Date(
-//       now.getFullYear(),
-//       now.getMonth() + 1,
-//       0,
-//     ).getDate()
-//     const greaterDateCron = parseExpression(`59 23 ${daysInMonth} * *`)
+export class CronDateUtils {
+  static getMonthGreaterDate = (): Date => {
+    const now = new Date()
+    const daysInMonth = new Date(
+      now.getFullYear(),
+      now.getMonth() + 1,
+      0,
+    ).getDate()
+    const greaterDateCron = parseExpression(`59 23 ${daysInMonth} * *`)
 
-//     return new Date(greaterDateCron.next().getTime())
-//   }
+    return new Date(greaterDateCron.next().getTime())
+  }
 
-//   static getMonthLowerDate = (): Date => {
-//     const lowerDateCron = parseExpression('0 0 1 * *')
+  static getMonthLowerDate = (): Date => {
+    const lowerDateCron = parseExpression('0 0 1 * *')
 
-//     return new Date(lowerDateCron.prev().getTime())
-//   }
+    return new Date(lowerDateCron.prev().getTime())
+  }
 
-//   static getWeekGreaterDate = (): Date => {
-//     const greaterDateCron = parseExpression('59 23 * * sun')
-//     return new Date(greaterDateCron.next().getTime())
-//   }
+  static getWeekGreaterDate = (): Date => {
+    const greaterDateCron = parseExpression('59 23 * * sun')
+    return new Date(greaterDateCron.next().getTime())
+  }
 
-//   static getWeekLowerDate = (): Date => {
-//     const lowerDateCron = parseExpression('0 0 * * mon')
-//     return new Date(lowerDateCron.prev().getTime())
-//   }
-// }
+  static getWeekLowerDate = (): Date => {
+    const lowerDateCron = parseExpression('0 0 * * mon')
+    return new Date(lowerDateCron.prev().getTime())
+  }
+}
--- functions/src/utils/utils.ts
+++ functions/src/utils/utils.ts
@@ -1,49 +1,49 @@
-// import { Reward } from '../models/reward'
-// import { FilterQueries } from './filter.queries'
+import { Reward } from '../models/reward'
+import { FilterQueries } from './filter.queries'
 
-// export class Utilities {
-//   static monthScore: (uid: string) => Promise<number> | undefined
-//   static weekScore: (uid: string) => Promise<number> | undefined
-//   static allTimeScore: (uid: string) => Promise<number> | undefined
-//   async allTimeScore(uid: string): Promise<number> {
-//     const result = await Reward.aggregate([
-//       { $match: FilterQueries.userAllTimeRewards(uid) },
-//       {
-//         $group: {
-//           _id: '$uid',
-//           total: { $sum: { $toInt: '$topaz' } },
-//         },
-//       },
-//     ])
-//     const score = result.length > 0 ? result[0]?.total : 0
-//     return score
-//   }
+export class Utilities {
+  static monthScore: (uid: string) => Promise<number> | undefined
+  static weekScore: (uid: string) => Promise<number> | undefined
+  static allTimeScore: (uid: string) => Promise<number> | undefined
+  async allTimeScore(uid: string): Promise<number> {
+    const result = await Reward.aggregate([
+      { $match: FilterQueries.userAllTimeRewards(uid) },
+      {
+        $group: {
+          _id: '$uid',
+          total: { $sum: { $toInt: '$topaz' } },
+        },
+      },
+    ])
+    const score = result.length > 0 ? result[0]?.total : 0
+    return score
+  }
 
-//   async monthScore(uid: string): Promise<number> {
-//     const result = await Reward.aggregate([
-//       { $match: FilterQueries.userMonthRewards(uid) },
-//       {
-//         $group: {
-//           _id: '$uid',
-//           total: { $sum: { $toInt: '$topaz' } },
-//         },
-//       },
-//     ])
-//     const score = result.length > 0 ? result[0]?.total : 0
-//     return score
-//   }
+  async monthScore(uid: string): Promise<number> {
+    const result = await Reward.aggregate([
+      { $match: FilterQueries.userMonthRewards(uid) },
+      {
+        $group: {
+          _id: '$uid',
+          total: { $sum: { $toInt: '$topaz' } },
+        },
+      },
+    ])
+    const score = result.length > 0 ? result[0]?.total : 0
+    return score
+  }
 
-//   async weekScore(uid: string): Promise<number> {
-//     const result = await Reward.aggregate([
-//       { $match: FilterQueries.userWeekRewards(uid) },
-//       {
-//         $group: {
-//           _id: '$uid',
-//           total: { $sum: { $toInt: '$topaz' } },
-//         },
-//       },
-//     ])
-//     const score = result.length > 0 ? result[0]?.total : 0
-//     return score
-//   }
-// }
+  async weekScore(uid: string): Promise<number> {
+    const result = await Reward.aggregate([
+      { $match: FilterQueries.userWeekRewards(uid) },
+      {
+        $group: {
+          _id: '$uid',
+          total: { $sum: { $toInt: '$topaz' } },
+        },
+      },
+    ])
+    const score = result.length > 0 ? result[0]?.total : 0
+    return score
+  }
+}
